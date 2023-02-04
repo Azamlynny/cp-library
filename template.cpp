627c833e1ba0d08129b3cfc1f2525bf68dd2b047
azamlynny <adamzamlynny@gmail.com>
2023-02-04 13:26:36 -0500

    Add general template

---
+++ template.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>  
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+typedef vector<int> vi;
+typedef vector<vector<int>> vvi;
+typedef vector<ll> vl;
+typedef vector<vector<ll>> vvl;
+#define pb push_back
+#define endl "\n" 
+// String	
+#define lowercase(s) transform((s).begin(), (s).end(), (s).begin(), ::tolower);  
+#define uppercase(s) transform((s).begin(), (s).end(), (s).begin(), ::toupper);
+  
+void solve() {
+	
+}
+  
+    
+int main() {
+    ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0); cout.precision(20);
+	int tc = 1;
+    // cin >> tc;
+    for (int t = 1; t <= tc; t++) {
+        solve();
+    }
+    return 0;
+} 
