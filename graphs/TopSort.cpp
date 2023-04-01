39771b2496155246e2e1e1fbef155fb83b33e839
Azamlynny <azamlynny@hotmail.com>
2023-04-01 17:39:09 -0400

    Add TopSort for edge, weight adj list with generic indexing

--- graphs/TopSort.cpp
+++ graphs/TopSort.cpp
@@ -1,7 +1,10 @@
- // Description: Finds topological sort order traversal for a DAG
- // Time: O(n + m)
+// Description: Finds topological sort order traversal for a DAG
+// Time: O(n + m)
+// Pasteable: FULL
 
-// Only Edges
+#define START_INDEX 1
+
+// Only Edges, v
 
 template<typename T>
 void dfsTopSort(vector<vector<T>>& adj, vector<bool>& vis, vector<T>& ts, T v) {
@@ -20,7 +23,7 @@ vector<T> topSort(vector<vector<T>>& adj) {
     vector<bool> vis(n,0);
     vector<T> ts;
 
-    for (T i = 0; i < n; i++) {
+    for (T i = START_INDEX; i < n; i++) {
         if(!vis[i]) {
             dfsTopSort(adj, vis, ts, i);
         }
@@ -29,7 +32,29 @@ vector<T> topSort(vector<vector<T>>& adj) {
     return ts;
 }
 
-// Edges + Weights
+// Edges + Weights, pair<v, w>
+template<typename T1, typename T2>
+void dfsTopSort(vector<vector<pair<T1, T2>>>& adj, vector<bool>& vis, vector<T1>& ts, T1 v) {
+    vis[v] = 1;
+    for (pair<T1, T2> uw: adj[v]) {
+        if (!vis[uw.first]) {
+            dfsTopSort(adj, vis, ts, uw.first);
+        }
+    }
+    ts.push_back(v);
+}
 
+template<typename T1, typename T2>
+vector<T1> topSort(vector<vector<pair<T1, T2>>>& adj) {
+    int n = adj.size();
+    vector<bool> vis(n,0);
+    vector<T1> ts;
 
-
+    for (T1 i = START_INDEX; i < n; i++) {
+        if(!vis[i]) {
+            dfsTopSort(adj, vis, ts, i);
+        }
+    }
+    reverse(ts.begin(), ts.end());
+    return ts;
+}
