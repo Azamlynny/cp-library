84108893b27a869a97f8a4f3cf96c9537e4444e6
Azamlynny <azamlynny@hotmail.com>
2023-04-01 17:40:01 -0400

    Fix spacing

--- graphs/TopSort.cpp
+++ graphs/TopSort.cpp
@@ -5,7 +5,6 @@
 #define START_INDEX 1
 
 // Only Edges, v
-
 template<typename T>
 void dfsTopSort(vector<vector<T>>& adj, vector<bool>& vis, vector<T>& ts, T v) {
     vis[v] = 1;
@@ -22,7 +21,6 @@ vector<T> topSort(vector<vector<T>>& adj) {
     int n = adj.size();
     vector<bool> vis(n,0);
     vector<T> ts;
-
     for (T i = START_INDEX; i < n; i++) {
         if(!vis[i]) {
             dfsTopSort(adj, vis, ts, i);
@@ -49,7 +47,6 @@ vector<T1> topSort(vector<vector<pair<T1, T2>>>& adj) {
     int n = adj.size();
     vector<bool> vis(n,0);
     vector<T1> ts;
-
     for (T1 i = START_INDEX; i < n; i++) {
         if(!vis[i]) {
             dfsTopSort(adj, vis, ts, i);
