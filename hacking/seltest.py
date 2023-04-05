7fe8ea90f1383a39fee76a68f8a7c43c14d2b573
Azamlynny <azamlynny@hotmail.com>
2023-04-04 20:41:13 -0400

    Add submission link scraping

--- hacking/seltest.py
+++ hacking/seltest.py
@@ -8,17 +8,26 @@ problem_id = 'A'
 page = 1
 max_page = None
 
-driver = webdriver.Chrome('./chromedriver')
+options = webdriver.ChromeOptions()
+options.add_experimental_option('excludeSwitches', ['enable-logging'])
+driver = webdriver.Chrome('./chromedriver',options=options)
 
 while True:
     url = "http://codeforces.com/contest/"+contest_id+"/status/"+problem_id+"/page/" + str(page) + "?order=BY_ARRIVED_DESC"
+    print ("Parsing page:", page)
     try:
         driver.get(url)
         source = requests.get(url).text
-        print(source)
-        break
+        soup = BeautifulSoup(source, 'html.parser')
+        submissions = soup.findAll('a', {'class':'view-source'})
+        
+        for submission in submissions:
+            submission_url = "http://codeforces.com" + submission['href']
+            print(submission_url)
     except:
-        print("failed page ", page)
+        print("Finished parsing on page:", page)
         break
 
     page+=1
+
+driver.quit()
