0bd6bbf5e3d102906400834ec4dc1f2a39672d55
Azamlynny <azamlynny@hotmail.com>
2023-04-04 21:36:06 -0400

    Add scraping c++ code

--- hacking/seltest.py
+++ hacking/seltest.py
@@ -2,16 +2,45 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from bs4 import BeautifulSoup
 import requests
+import os
 
 contest_id = '1811'
-problem_id = 'A'
-page = 1
-max_page = None
+problem_id = 'F'
+directory = contest_id + '/' + problem_id
+
+replace = {'&quot;': '\"', '&gt;': '>', '&lt;': '<', '&amp;': '&', "&apos;": "'"}
+languages = {'GNU C++20 (64)', 'GNU C++14', 'GNU C++11', 'GNU C++17'} # , 'Java 8':'Java 11.0.5', 'Python 3': 'Python 3.7.2', 'Python 2':'Python 2.7.15'
+
+def parse(code):
+	for key in replace.keys():
+		code = code.replace(key, replace[key])
+	return code
+
+#fetches the code corresponding to the parameter url
+def getCode(submission_url, submission_id, contest_id):
+    source = requests.get(submission_url).text
+    soup = BeautifulSoup(source, 'html.parser')
+    language = soup.findAll('td')[3].text[6:-6]
+    # print(language)
+    if language not in languages:
+        return
+    code = soup.findAll('pre')[0].text
+    code = parse(code).replace('\r', '')
+    if code != '':
+        print("Create", submission_id + ".cpp")
+        filewrite = open(directory + '/' + submission_id + '.cpp', 'w')
+        filewrite.write(code)
+        filewrite.close()
+
+
+if (not os.path.exists(directory)):
+    os.makedirs(directory)
 
 options = webdriver.ChromeOptions()
 options.add_experimental_option('excludeSwitches', ['enable-logging'])
 driver = webdriver.Chrome('./chromedriver',options=options)
 
+page = 1
 while True:
     url = "http://codeforces.com/contest/"+contest_id+"/status/"+problem_id+"/page/" + str(page) + "?order=BY_ARRIVED_DESC"
     print ("Parsing page:", page)
@@ -23,10 +52,14 @@ while True:
         
         for submission in submissions:
             submission_url = "http://codeforces.com" + submission['href']
-            print(submission_url)
+            try:
+                getCode(submission_url, submission['submissionid'], contest_id)
+            except:
+                continue
+
     except:
         print("Finished parsing on page:", page)
-        break
+        # break
 
     page+=1
 
