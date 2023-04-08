f6dcd301a29c4c801c9f6db65d01652c6d3ad1b2
Azamlynny <azamlynny@hotmail.com>
2023-04-08 09:56:44 -0400

    Update binary search

--- implementations/other/BinarySearch.h
+++ implementations/other/BinarySearch.h
@@ -1,29 +1,29 @@
 // Time: O(log n)
 
-long long lowerBound(int x) {
-    long long l = 0;
-    long long r = n;
-    while (l < r) {
-        int m = l + (r - l) / 2;
-        if (arr[m] < x) {
-            l = m + 1;
-        } else {
-            r = m;
-        }
-    }
-    return l;
-}
+// long long lowerBound(int x) {
+long long l = 0;
+long long r = n;
+while (l < r) {
+    long long m = (r - l) / 2 + l;
 
-int upperBound(int x) {
-    int l = 0;
-    int r = n;
-    while (l < r) {
-        int m = l + (r - l) / 2;
-        if (arr[m] > x) {
-            r = m;
-        } else {
-            l = m + 1;
-        }
+    if (arr[m] < x) {
+        l = m + 1;
+    } else {
+        r = m;
     }
-    return l - 1;
 }
+return l;
+
+// int upperBound(int x) {
+long long l = 0;
+long long r = n;
+while (l < r) {
+    long long m = (r - l) / 2 + l;
+
+    if (arr[m] > x) {
+        r = m;
+    } else {
+        l = m + 1;
+    }
+}
+return l - 1;
